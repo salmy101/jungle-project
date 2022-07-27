95e1faee02990c9bb6a8ccc4f8a11605b62b0ed1
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-07-27 18:45:52 +0000

    Initial commit and  project setup

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
+    #uses MODEL
     @products = Product.all.order(created_at: :desc)
+
+    #renders VIEW 
+    render :index
   end
 
   def show
