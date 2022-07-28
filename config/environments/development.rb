4552b0c7062e8dd8caafd0caa01b475a0d7a8786
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-07-28 03:00:17 +0000

    add order list table, fix postion of quant, and price, add total price

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::FileUpdateChecker
 
 
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
