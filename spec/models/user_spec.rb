91f131f6c74db210c3fbe614f4870c149d58cb3e
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-08-02 02:52:21 +0000

    add user spec in models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+
+    it "must be created with an password and password_confirmation field" do 
+      
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  end
+end
